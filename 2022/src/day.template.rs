6d53ad4656fb6b5911411b25bef0e54f4c720e15
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-12-03 20:30:21 +0100

    day template

---
+++ 2022/src/day.template.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_input() -> &'static str {
+        include_str!("../puzzle_input/day.txt")
+    }
+
+    #[test]
+    #[ignore]
+    fn test_example_1() {
+        const INPUT: &str = r"
+        
+        ";
+        assert!(false);
+        // const ANSWER1: u32 = u32::MAX;
+        // assert_eq!(process_1(INPUT), ANSWER1);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_problem_1() {
+        let input = get_input();
+
+        assert!(false);
+        // const ANSWER1: u32 = u32::MAX;
+        // assert_eq!(process_1(input), ANSWER1);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_example_2() {
+        const INPUT: &str = r"
+        
+        ";
+        assert!(false);
+        // const ANSWER1: u32 = u32::MAX;
+        // assert_eq!(process_1(INPUT), ANSWER1);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_problem_2() {
+        let input = get_input();
+
+        assert!(false);
+        // const ANSWER: u32 = u32::MAX;
+        // assert_eq!(process_2(input), ANSWER2);
+    }
+}
