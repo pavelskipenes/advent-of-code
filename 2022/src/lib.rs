36b43b9a5670b387f489f42fb4074042917fe6cc
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-12-04 19:01:23 +0100

    add stricter lints

--- 2022/src/lib.rs
+++ 2022/src/lib.rs
@@ -1,9 +1,14 @@
-#![warn(clippy::pedantic)]
-#![warn(clippy::perf)]
-#![warn(clippy::correctness)]
-#![warn(clippy::suspicious)]
-#![warn(clippy::style)]
+#![deny(clippy::pedantic)]
+#![deny(clippy::perf)]
+#![deny(clippy::correctness)]
+#![deny(clippy::suspicious)]
+#![deny(clippy::style)]
+#![deny(deprecated)]
+#![deny(unknown_lints)]
+#![deny(future_incompatible)]
 #![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(dead_code)]
+#![deny(unused)]
 
 pub mod day_1;
 pub mod day_2;
