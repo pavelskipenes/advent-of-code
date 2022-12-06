f04481800a35d633e298ebf9fa2e2edc28d2c273
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-12-06 14:17:06 +0100

    day 6: add puzzle input

---
+++ 2022/src/day_6.rs
@@ -0,0 +1,66 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const fn get_input() -> &'static str {
+        include_str!("../puzzle_input/day_6.txt")
+    }
+
+    const fn get_example_input_1() -> &'static str {
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    }
+    const fn get_example_input_2() -> &'static str {
+        "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    }
+    const fn get_example_input_3() -> &'static str {
+        "nppdvjthqldpwncqszvftbrmjlhg"
+    }
+    const fn get_example_input_4() -> &'static str {
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    }
+    const fn get_example_input_5() -> &'static str {
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    }
+
+    #[test]
+    #[ignore]
+    fn test_example_1() {
+        const INPUT: &str = r"
+        
+        ";
+        assert!(false);
+        // const ANSWER1: u32 = u32::MAX;
+        // assert_eq!(process_1(INPUT), ANSWER1);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_problem_1() {
+        let input = get_input();
+
+        assert!(false);
+        // const ANSWER1: u32 = u32::MAX;
+        // assert_eq!(process_1(input), ANSWER1);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_example_2() {
+        const INPUT: &str = r"
+        
+        ";
+        assert!(false);
+        // const ANSWER1: u32 = u32::MAX;
+        // assert_eq!(process_1(INPUT), ANSWER1);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_problem_2() {
+        let input = get_input();
+
+        assert!(false);
+        // const ANSWER: u32 = u32::MAX;
+        // assert_eq!(process_2(input), ANSWER2);
+    }
+}
--- 2022/src/lib.rs
+++ 2022/src/lib.rs
@@ -15,3 +15,4 @@ pub mod day_2;
 pub mod day_3;
 pub mod day_4;
 pub mod day_5;
+pub mod day_6;
