6680b647b01073b161ef85c88d418729e7528ee6
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2023-12-14 13:40:24 +0100

    2023,1,2: refactor
    - move common functions to shared "scope"
    - delete useless funtions

--- 2023/lib/day1.ex
+++ 2023/lib/day1.ex
@@ -1,50 +1,160 @@
 defmodule Day1 do
   use ExUnit.Case
 
-  def solver_part1(input) when is_binary(input) do
-    String.split(input, "\n")
-    |> Enum.map(&convert_number/1)
-    |> Enum.reduce(0, fn (element, accumulator) -> accumulator + element end)
+  def first_and_last(list) when is_list(list) do
+    [List.first(list), List.last(list)]
   end
 
-  defp convert_number(line) when is_binary(line) do
-    line
-    |> String.to_charlist
-    |> Enum.filter(&is_ascii_number/1)
-    |> first_and_last 
-    |> List.to_string
-    |> String.to_integer
+  # convert a list as if were one integer. First index is the most significant digit.
+  def to_integer(list) when is_list(list) do
+    list
+    |> Enum.reverse()
+    |> to_integer(0)
   end
 
-  def string_to_number(string) do
-    case string do
-      "one" -> 1
-      "two" -> 2
-      "three" -> 3
-      "four" -> 4
-      "five" -> 5
-      "six" -> 6
-      "seven" -> 7
-      "eight" -> 8
-      "nine" -> 9
-      "zero" -> 0
-      _ -> :no_match
+  defp to_integer(list, power) when is_list(list) and is_integer(power) do
+    case list do
+      [] -> 0
+      [head | tail] when is_integer(head) -> head * :math.pow(10, power) + to_integer(tail, power + 1)
     end
   end
 
-  defp is_ascii_number(char) when char in ?0..?9 do
-    true
-  end 
+  defmodule Part1 do
+    def solve(input) when is_binary(input) do
+      input
+      |> String.trim
+      |> String.split("\n")
+      |> Enum.map(&line_solver/1)
+      |> Enum.sum
+    end
 
-  defp is_ascii_number(_char) do
-    false
-  end 
-
-  defp first_and_last(list) when is_list(list) and length(list) >= 1 do
-    [List.first(list) , List.last(list)]
+    defp line_solver(line) when is_binary(line) do
+      line
+      |> String.to_charlist() # list of unicode numbers
+      |> Enum.filter(fn char -> char in ?1..?9 end) # filter out numerical ascii values
+      |> Day1.first_and_last
+      |> Enum.map(fn el -> el - 0x30 end) # ascii number to number
+      |> Day1.to_integer
+    end
   end
 
-  defp first_and_last(list) when is_list(list) and length(list) == 0 do
-    [?0]
+  defmodule Part2 do
+    def solve(input) when is_binary(input) do
+      input
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(&line_solver/1)
+      |> Enum.sum
+    end
+
+    # this solver walkes the line and will either discard one character at a time or multiple if they match an expected value. If the input has overlapping values like "oneight" then it will only find "one" and the remainder will be "ight" which will not match "eight"
+    def line_solver(line) do
+      line
+      |> String.graphemes()
+      |> extract_numbers
+      |> Day1.first_and_last
+      |> Enum.map(fn string -> Integer.parse(string) end)
+      |> Enum.map(fn result -> {value, _} = result; value end)
+      |> Day1.to_integer
+    end
+
+
+    def string_to_number(string) when is_binary(string) do
+      case string do
+        "one" -> {:ok, 1}
+        "two" -> {:ok, 2}
+        "three" -> {:ok, 3}
+        "four" -> {:ok, 4}
+        "five" -> {:ok, 5}
+        "six" -> {:ok, 6}
+        "seven" -> {:ok, 7}
+        "eight" -> {:ok, 8}
+        "nine" -> {:ok, 9}
+        "1" -> {:ok, 1}
+        "2" -> {:ok, 2}
+        "3" -> {:ok, 3}
+        "4" -> {:ok, 4}
+        "5" -> {:ok, 5}
+        "6" -> {:ok, 8}
+        "7" -> {:ok, 7}
+        "8" -> {:ok, 8}
+        "9" -> {:ok, 9}
+        _ -> {:error, "no match"}
+      end
+    end
+
+    def convert_number(line) when is_binary(line) do
+      line
+      |> possible_string_permutations(3..5)
+      |> Enum.map(&string_to_number/1)
+      |> Enum.filter(fn
+        {:ok, _} -> true
+        _ -> false
+      end)
+      |> Enum.map(fn {:ok, val} -> val end)
+      |> Day1.to_integer
+    end
+
+
+    def possible_string_permutations(line, window_size) when is_binary(line) do
+      window_size
+      |> Enum.map(fn window_size -> windows(String.to_charlist(line), window_size) end)
+      |> Enum.map(&string_to_number/1)
+      |> List.flatten()
+    end
+
+    def valid_permutations(string_permutations) do
+      string_permutations
+      |> Enum.map(fn potential_number -> string_to_number(potential_number) end)
+      |> Enum.filter(fn
+        {:ok, _} -> true
+        _ -> false
+      end)
+      |> Enum.map(fn {_, value} -> value end)
+    end
+
+    def windows(list, size) do
+      windows_slice_indexies(list, size)
+      |> Enum.map(fn starting_index -> Enum.slice(list, starting_index, size) end)
+    end
+
+    def windows_slice_indexies(list, size)
+      when is_list(list) and size >= 1 and length(list) >= size do
+      0..(length(list) - size)
+    end
+
+    def strings_to_number(list_of_strings) when is_list(list_of_strings) do
+      list_of_strings
+      |> Enum.map(&string_to_number/1)
+    end
+
+    def extract_numbers(graphemes) when is_list(graphemes) do
+      case graphemes do
+        [] -> []
+        [number | rest] when number in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] -> [number | extract_numbers(rest)]
+        ["o", "n", "e" | rest] ->
+          ["1" | extract_numbers(rest)]
+        ["t", "w", "o" | rest] ->
+          ["2" | extract_numbers(rest)]
+        ["t", "h", "r", "e", "e" | rest] ->
+          ["3" | extract_numbers(rest)]
+        ["f", "o", "u", "r" | rest] ->
+          ["4" | extract_numbers(rest)]
+        ["f", "i", "v", "e" | rest] ->
+          ["5" | extract_numbers(rest)]
+        ["s", "i", "x" | rest] ->
+          ["6" | extract_numbers(rest)]
+        ["s", "e", "v", "e", "n" | rest] ->
+          ["7" | extract_numbers(rest)]
+        ["e", "i", "g", "h", "t" | rest] ->
+          ["8" | extract_numbers(rest)]
+        ["n", "i", "n", "e" | rest] ->
+          ["9" | extract_numbers(rest)]
+        [_ | rest] ->
+          extract_numbers(rest)
+      end
+    end
+
   end
+
 end
--- 2023/test/day1_test.exs
+++ 2023/test/day1_test.exs
@@ -3,27 +3,81 @@ defmodule Day1Test do
   doctest Day1
 
   test "test puzzle one" do
-    {result, file_content} = File.read("puzzle_input/day_1_test.txt")
+    {result, file_content} = File.read("puzzle_input/day_1/part1_test.txt")
     assert result == :ok
-    assert 142 == Day1.solver_part1(file_content)
+    assert 142 == Day1.Part1.solve(file_content)
   end
 
   test "problem 1" do
-    assert 55208 == File.read!("puzzle_input/day_1.txt")
-    |> Day1.solver_part1
+    assert 55208 ==
+      File.read!("puzzle_input/day_1/input.txt")
+      |> Day1.Part1.solve()
   end
 
   test "number matching" do
-    assert Day1.string_to_number("one") == 1
-    assert Day1.string_to_number("two") == 2
-    assert Day1.string_to_number("three") == 3
-    assert Day1.string_to_number("four") == 4
-    assert Day1.string_to_number("five") == 5
-    assert Day1.string_to_number("six") == 6
-    assert Day1.string_to_number("seven") == 7
-    assert Day1.string_to_number("eight") == 8
-    assert Day1.string_to_number("nine") == 9
-    assert Day1.string_to_number("zero") == 0
+    assert Day1.Part2.string_to_number("one") == {:ok, 1}
+    assert Day1.Part2.string_to_number("two") == {:ok, 2}
+    assert Day1.Part2.string_to_number("three") == {:ok, 3}
+    assert Day1.Part2.string_to_number("four") == {:ok, 4}
+    assert Day1.Part2.string_to_number("five") == {:ok, 5}
+    assert Day1.Part2.string_to_number("six") == {:ok, 6}
+    assert Day1.Part2.string_to_number("seven") == {:ok, 7}
+    assert Day1.Part2.string_to_number("eight") == {:ok, 8}
+    assert Day1.Part2.string_to_number("nine") == {:ok, 9}
   end
 
+  test "windows size" do
+    assert 0..1 == Day1.Part2.windows_slice_indexies([1, 2], 1)
+    assert 0..0 == Day1.Part2.windows_slice_indexies([1, 2], 2)
+    assert 0..4 == Day1.Part2.windows_slice_indexies([1, 2, 3, 4, 5, 6], 2)
+  end
+
+  test "windows" do
+    [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]] = Day1.Part2.windows([1, 2, 3, 4, 5, 6], 2)
+  end
+
+  test "to_integer" do
+    assert 1 == Day1.to_integer([1])
+    assert 12 == Day1.to_integer([1, 2])
+    assert 123 == Day1.to_integer([1, 2, 3])
+  end
+
+  test "extract numbers" do
+    value = "123"
+      |> String.graphemes
+      |> Day1.Part2.extract_numbers
+      |> Enum.map(fn number -> Integer.parse(number) end)
+      |> Enum.map(fn {val, ""} -> val end)
+      |> Day1.to_integer
+    assert value == 123
+    value = "one"
+      |> String.graphemes
+      |> Day1.Part2.extract_numbers
+      |> IO.inspect
+      |> Enum.map(fn number -> Integer.parse(number) end)
+      |> Enum.map(fn {val, ""} -> val end)
+      |> Day1.to_integer
+    assert value == 1
+
+    value = "2oneightg"
+      |> String.graphemes()
+      |> Day1.Part2.extract_numbers
+      |> IO.inspect
+      |> Enum.map(fn number -> Integer.parse(number) end)
+      |> Enum.map(fn {val, ""} -> val end)
+      |> Day1.to_integer
+    assert value == ["2", "1", "8"]
+  end
+
+  # test "part 2 test input" do
+  #   assert 281 ==
+  #            File.read!("puzzle_input/day_1/part2_test.txt")
+  #            |> Day1.Part2.solve()
+  # end
+  #
+  # test "part 2 prod input" do
+  #   assert 54558 <
+  #            File.read!("puzzle_input/day_1/input.txt")
+  #            |> Day1.Part2.solve()
+  # end
 end
