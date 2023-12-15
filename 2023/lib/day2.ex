5d56e49dc3ca439d6b3529c1307d73b85a8e5d06
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2023-12-15 16:25:39 +0100

    2023,2,1: completed

--- 2023/lib/day2.ex
+++ 2023/lib/day2.ex
@@ -1,15 +1,90 @@
 defmodule Day2 do
-
   defmodule Part1 do
-    def solve(input) when is_binary(input) do
+    def solve(input, constraint) when is_binary(input) when is_list(constraint) do
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&game_parser/1)
+      |> Enum.filter(fn game -> game_possible(game, constraint) end)
+      |> Enum.map(fn game -> game[:id] end)
+      |> Enum.sum
+    end
 
+    def game_possible(game, constraint) when is_map(game) and is_list(constraint) do
+      invalid_samples = game[:samples]
+        |> Enum.filter(fn sample -> not sample_valid(sample, constraint) end)
+        |> Enum.count
+
+      invalid_samples === 0
+    end
+
+    def sample_valid(sample, constraint) when is_list(sample) and is_list(constraint) do
+      cond do
+        Keyword.has_key?(sample, :red) and Keyword.get(sample, :red) > Keyword.get(constraint, :red) -> false
+        Keyword.has_key?(sample, :green) and Keyword.get(sample, :green) > Keyword.get(constraint,:green) -> false
+        Keyword.has_key?(sample, :blue) and Keyword.get(sample, :blue) >  Keyword.get(constraint,:blue) -> false
+        true -> true
+      end
+    end
+
+    def game_parser(game) when is_binary(game) do
+      samples = game
+        |> String.split(":")
+        |> Enum.reverse()
+        |> List.first()
+        |> extract_game_samples
+
+      id = game
+        |> String.split(":")
+        |> List.first()
+        |> extract_id
+
+      %{:id => id, :samples => samples}
+    end
+
+    def extract_game_samples(game_samples) when is_binary(game_samples) do
+      game_samples
+      |> String.trim()
+      |> String.split(";")
+      |> Enum.map(&extract_game_sample/1)
+    end
+
+    def extract_game_sample(input) when is_binary(input) do
+      input
+      |> String.split(",")
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&count_cubes/1)
+      |> Enum.map(fn element ->
+        {color, {amount, _}} = element
+        {color, amount}
+      end)
+    end
+
+    def count_cubes(color_and_count) when is_binary(color_and_count) do
+      count =
+        color_and_count
+        |> String.split(" ")
+        |> List.first()
+
+      color = String.slice(color_and_count, (String.length(count) + 1)..-1)
+
+      case color do
+        "red" -> {:red, Integer.parse(count)}
+        "green" -> {:green, Integer.parse(count)}
+        "blue" -> {:blue, Integer.parse(count)}
+      end
+    end
+
+    def extract_id(game_part) when is_binary(game_part) do
+      case game_part do
+        "Game " <> id -> id
+      end
+      |> Integer.parse
+      |> Kernel.then(fn result -> {val, _} = result; val end)
     end
   end
 
   defmodule Part2 do
-    def solve(input) when is_binary(input) do
-
+    def solve(input, constraint) when is_binary(input) and is_list(constraint) do
     end
   end
-
 end
--- 2023/test/day2_test.exs
+++ 2023/test/day2_test.exs
@@ -1,17 +1,52 @@
 defmodule Day2Test do
   use ExUnit.Case
 
-  test "part 1 test" do
-    assert 8 == AdventOfCode.get_puzzle_test(2,1)
-    |> Day2.Part1.solve
+  test "part 1 sample invalid" do
+    constraint = [{:red, 12}, {:green, 13}, {:blue, 14}]
+
+    sample = [{:blue, 3}, {:red, 4}]
+    assert Day2.Part1.sample_valid(sample, constraint) === true
+
+    sample = [{:red, 1}, {:green, 2}, {:blue, 16}]
+    assert Day2.Part1.sample_valid(sample, constraint) === false
+
+    sample = [{:green, 2}]
+    assert Day2.Part1.sample_valid(sample, constraint) === true
   end
 
-  # test "part 1" do
-  #   AdventOfCode.get_puzzle(2)
-  #   |> Part1.solve
-  #   |> IO.inspect
-  # end
-  #
+  test "part 1 game possible" do
+    constraint = [red: 12, green: 13, blue: 14 ]
+
+    game1 = %{id: 1, samples: [[blue: 3, red: 4], [red: 1, green: 2, blue: 6], [green: 2]]}
+    assert Day2.Part1.game_possible(game1, constraint) === true
+
+    game2 = %{id: 2, samples: [[blue: 1, green: 2], [green: 3, blue: 4, red: 1], [green: 1, blue: 1]]}
+    assert Day2.Part1.game_possible(game2, constraint) === true
+
+    game3 = %{id: 3, samples: [[green: 8, blue: 6, red: 20], [blue: 5, red: 4, green: 13], [green: 5, red: 1]]}
+    assert Day2.Part1.game_possible(game3, constraint) === false
+
+    game4 = %{id: 4, samples: [[green: 1, red: 3, blue: 6], [green: 3, red: 6], [green: 3, blue: 15, red: 14]]}
+    assert Day2.Part1.game_possible(game4, constraint) === false
+
+    game5 = %{id: 5, samples: [[red: 6, blue: 1, green: 3], [blue: 2, red: 1, green: 2]]}
+    assert Day2.Part1.game_possible(game5, constraint) === true
+  end
+
+  test "part 1 test" do
+    constraint = [{:red, 12}, {:green, 13}, {:blue, 14}]
+    assert 8 ==
+      AdventOfCode.get_puzzle_test(2, 1)
+      |> Day2.Part1.solve(constraint)
+  end
+
+  test "part 1" do
+    constraint = [{:red, 12}, {:green, 13}, {:blue, 14}]
+
+    assert 1931 == AdventOfCode.get_puzzle(2)
+      |> Day2.Part1.solve(constraint)
+  end
+
   # test "part 2 test" do
   #   assert VAL == AdventOfCode.get_puzzle_test(2, 2)
   #   |> Part2.solve
