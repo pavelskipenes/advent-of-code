97326cc6d58af1c0ff3fcb995e699044b3dade65
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2023-12-15 22:32:57 +0100

    2023,2,2: completed

--- 2023/lib/day2.ex
+++ 2023/lib/day2.ex
@@ -5,40 +5,31 @@ defmodule Day2 do
       |> String.split("\n", trim: true)
       |> Enum.map(&game_parser/1)
       |> Enum.filter(fn game -> game_possible(game, constraint) end)
-      |> Enum.map(fn game -> game[:id] end)
+      |> Enum.map(fn game -> {id, _samples} = game; id end)
       |> Enum.sum
     end
 
-    def game_possible(game, constraint) when is_map(game) and is_list(constraint) do
-      invalid_samples = game[:samples]
-        |> Enum.filter(fn sample -> not sample_valid(sample, constraint) end)
-        |> Enum.count
-
-      invalid_samples === 0
-    end
-
-    def sample_valid(sample, constraint) when is_list(sample) and is_list(constraint) do
-      cond do
-        Keyword.has_key?(sample, :red) and Keyword.get(sample, :red) > Keyword.get(constraint, :red) -> false
-        Keyword.has_key?(sample, :green) and Keyword.get(sample, :green) > Keyword.get(constraint,:green) -> false
-        Keyword.has_key?(sample, :blue) and Keyword.get(sample, :blue) >  Keyword.get(constraint,:blue) -> false
-        true -> true
-      end
-    end
-
     def game_parser(game) when is_binary(game) do
-      samples = game
-        |> String.split(":")
+      game_samples = game |> String.split(":")
+
+      id = game_samples
+        |> List.first()
+        |> extract_id
+
+      samples = game_samples
         |> Enum.reverse()
         |> List.first()
         |> extract_game_samples
 
-      id = game
-        |> String.split(":")
-        |> List.first()
-        |> extract_id
+      {id, samples}
+    end
 
-      %{:id => id, :samples => samples}
+    def extract_id(game_part) when is_binary(game_part) do
+      case game_part do
+        "Game " <> id -> id
+      end
+      |> Integer.parse
+      |> Kernel.then(fn result -> {val, _} = result; val end)
     end
 
     def extract_game_samples(game_samples) when is_binary(game_samples) do
@@ -52,39 +43,76 @@ defmodule Day2 do
       input
       |> String.split(",")
       |> Enum.map(&String.trim/1)
-      |> Enum.map(&count_cubes/1)
-      |> Enum.map(fn element ->
-        {color, {amount, _}} = element
-        {color, amount}
-      end)
+      |> Enum.map(&color_count/1)
     end
 
-    def count_cubes(color_and_count) when is_binary(color_and_count) do
-      count =
-        color_and_count
-        |> String.split(" ")
-        |> List.first()
+    def color_count(color_and_count) when is_binary(color_and_count) do
+      [ count | color_text ] = color_and_count |> String.split(" ")
+      {count, _} = Integer.parse(count)
 
-      color = String.slice(color_and_count, (String.length(count) + 1)..-1)
-
-      case color do
-        "red" -> {:red, Integer.parse(count)}
-        "green" -> {:green, Integer.parse(count)}
-        "blue" -> {:blue, Integer.parse(count)}
+      case List.first(color_text) do
+        "red" -> {:red, count}
+        "green" -> {:green, count}
+        "blue" -> {:blue, count}
       end
     end
 
-    def extract_id(game_part) when is_binary(game_part) do
-      case game_part do
-        "Game " <> id -> id
+    def game_possible(game, constraint) when is_tuple(game) and is_list(constraint) and tuple_size(game) == 2 do
+      {_id, samples} = game
+      invalid_samples = samples
+        |> Enum.filter(fn samples -> not hand_valid(samples, constraint) end)
+        |> Enum.count
+
+      invalid_samples === 0
+    end
+
+    def hand_valid(hand, constraints) when is_list(hand) and is_list(constraints) do
+      0 === hand
+        |> Enum.filter(fn color_amount -> not color_count_valid(color_amount, constraints) end)
+        |> Enum.count
+    end
+
+    def color_count_valid(color_count, constraints) when is_tuple(color_count) and tuple_size(color_count) == 2 and is_list(constraints) do
+      {color, count} = color_count
+      cond do
+        Keyword.has_key?(constraints, color) -> not (count > Keyword.get(constraints, color))
+        true -> true
       end
-      |> Integer.parse
-      |> Kernel.then(fn result -> {val, _} = result; val end)
     end
   end
 
   defmodule Part2 do
-    def solve(input, constraint) when is_binary(input) and is_list(constraint) do
+    def solve(input) when is_binary(input) do
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&Part1.game_parser/1)
+      |> Enum.map(&min_sample_set/1)
+      |> Enum.map(&power/1)
+      |> Enum.sum
+    end
+
+    def min_sample_set(game) when is_tuple(game) and tuple_size(game) == 2 do
+      accumulator = [{:red, nil}, {:green, nil}, {:blue, nil}]
+
+      {_id, samples} = game
+      samples
+      |> List.flatten
+      |> Enum.reduce(accumulator,
+        fn color_amount, accumulator ->
+          accumulator |> Enum.map(fn constraint ->
+            case {constraint, color_amount} do
+              {{color, nil}, {color, amount}} when is_atom(color) and is_integer(amount) -> {color, amount}
+              {{color, amount1}, {color, amount2}} when is_atom(color) and is_integer(amount1) and is_integer(amount2) -> {color, max(amount1, amount2)}
+              _ -> constraint
+            end
+          end)
+      end)
+    end
+
+    def power(max_values) when is_list(max_values) do
+      max_values
+      |> Enum.map(fn sample -> {_color, count} = sample; count end)
+      |> Enum.reduce(1, fn el, acc -> acc*el end)
     end
   end
 end
