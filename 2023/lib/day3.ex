2282f99bb6f952928e70a11674b7fb28f882dc18
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2023-12-15 23:38:34 +0100

    2023,3,1: extract index of chars inside line

--- 2023/lib/day3.ex
+++ 2023/lib/day3.ex
@@ -2,7 +2,30 @@ defmodule Day3 do
   defmodule Part1 do
     def solve(input) when is_binary(input) do
     end
+
+    def extract_symbols(line) when is_binary(line) do
+      {indexies, _} =
+        line
+        |> String.graphemes()
+        |> Enum.reduce({[], 0}, fn character, acumulator ->
+          {indexies, iteration} = acumulator
+
+          indexies =
+            case character do
+              "*" -> [iteration | indexies]
+              "$" -> [iteration | indexies]
+              "#" -> [iteration | indexies]
+              "+" -> [iteration | indexies]
+              _ -> indexies
+            end
+
+          {indexies, iteration + 1}
+        end)
+
+      indexies
+    end
   end
+
   defmodule Part2 do
     def solve(input) when is_binary(input) do
     end
--- 2023/test/day3_test.exs
+++ 2023/test/day3_test.exs
@@ -1,6 +1,14 @@
 defmodule Day3Test do
   use ExUnit.Case
 
+  test "test symbol extraction" do
+    assert [3] == Day3.Part1.extract_symbols("...*......\n")
+    assert [3] == Day3.Part1.extract_symbols("...$......\n")
+    assert [6, 3] == Day3.Part1.extract_symbols("...*..$...\n")
+    assert [7, 3] == Day3.Part1.extract_symbols("...*...+..\n")
+    assert [] == Day3.Part1.extract_symbols("..........\n")
+  end
+
   test "part 1 test" do
     assert 4361 == AdventOfCode.get_puzzle_test(3, 1)
     |> Day3.Part1.solve
